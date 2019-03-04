9a6263df5dafed500116853d395c3d67a090344e
fioriecheyenne <37355941+fioriecheyenne@users.noreply.github.com>
2019-03-03 20:48:35 -0700

    Update logic.js

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,19 +1,4 @@
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
 
-// Then bind the event to the callback function.
-// There are several events for cross browser compatibility.
-script.onreadystatechange = handler;
-script.onload = handler;
-
-// Fire the loading
-head.appendChild(script);
-
-function handler() {
-    console.log('jquery added :)');
-}
 // Words to guess
 var wordsArr = [
     "kaonashi", "calcifer", "totoro", "kodama", "ponyo", "haku", "howl", "susuwatari", "kamaji", "catbus", "jiji", "san", "arrietty", "turniphead"
